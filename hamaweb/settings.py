c5052b61d8e45510b76dff345dc8e6356d6d5a04
Frogkiller <frogkillerpl@gmail.com>
2020-06-02 19:00:21 +0200

    temp solution for debug on remote server in allow hosts

--- hamaweb/settings.py
+++ hamaweb/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config_json["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
